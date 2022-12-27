9acadfb3efe3c4d3e1e16ee6534dfa0fb3d0793d
Buse Yıldız <102406546+buseyildiz@users.noreply.github.com>
2022-12-27 20:53:01 +0300

    simple-image-gallery

---
+++ src/images.js
@@ -0,0 +1,9 @@
+const Images= [
+"https://images.unsplash.com/photo-1668111544643-179492f018a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+"https://images.unsplash.com/photo-1668372209893-9ef3d598e3dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+"https://images.unsplash.com/photo-1584798775282-11d379b8d05f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+"https://images.unsplash.com/photo-1564136363553-aafe8860ead1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+"https://images.unsplash.com/photo-1564136814675-54b861492414?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+]
+
+export default Images
